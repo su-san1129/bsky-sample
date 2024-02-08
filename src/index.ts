0ca8a2c8cd87d2dc27d09760cb2e9c1c1db41b3b
su-san1129 <deviationvalue1023@outlook.jp>
2024-02-08 22:54:03 +0900

    TEST post method

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,16 @@ import AtProtoAPI from '@atproto/api'
 const { BskyAgent } = AtProtoAPI
 
 const agent = new BskyAgent({ service: 'https://bsky.social' })
+
 const identifier = Deno.env.get('BSKY_IDENTIFIER') || ''
 const password = Deno.env.get('BSKY_PASSWORD') || ''
-const hoge = await agent.login({
+
+await agent.login({
     identifier,
     password
   })
-console.log(hoge)
+  
+await agent.post({
+  text: 'API を用いた投稿テストです。',
+  createdAt: new Date().toISOString()
+})
