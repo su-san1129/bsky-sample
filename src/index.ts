5df0c6d46c053e94082c9b6fbf4e5433551489d7
su-san1129 <deviationvalue1023@outlook.jp>
2024-02-13 00:06:38 +0900

    タイムラインの取得

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import 'https://deno.land/std@0.193.0/dotenv/load.ts';
-import AtProtoAPI from '@atproto/api'
-const { BskyAgent } = AtProtoAPI
+import "https://deno.land/std@0.193.0/dotenv/load.ts";
+import AtProtoAPI from "@atproto/api";
+const { BskyAgent } = AtProtoAPI;
 
-const agent = new BskyAgent({ service: 'https://bsky.social' })
+const agent = new BskyAgent({ service: "https://bsky.social" });
 
-const identifier = Deno.env.get('BSKY_IDENTIFIER') || ''
-const password = Deno.env.get('BSKY_PASSWORD') || ''
+const identifier = Deno.env.get("BSKY_IDENTIFIER") || "";
+const password = Deno.env.get("BSKY_PASSWORD") || "";
 
 await agent.login({
-    identifier,
-    password
-  })
-  
-await agent.post({
-  text: 'API を用いた投稿テストです。',
-  createdAt: new Date().toISOString()
-})
+  identifier,
+  password,
+});
+
+const tl = await agent.getTimeline({ limit: 10 });
+tl.data.feed.forEach(({ post }) => {
+  console.log(post.record);
+});
